570ce4725c044e53a8fdd4bad648c83ed8e1c20e
max5961 <mmorrissey5961@gmail.com>
2024-06-18 02:10:51 -0400

    Feat: ordering

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,8 +1,32 @@
 import { useState } from "react";
 import { useInput, useApp } from "ink";
-import DB, { Task, Tasks } from "../db/DB.js";
+import { Task, Tasks } from "../db/DB.js";
 import Util from "../common/Util.js";
 import Fetch from "./Fetch.js";
+
+export type OrderTasks =
+    | "NONE"
+    | "INC_AO"
+    | "DEC_AO"
+    | "PRIORITY_LH"
+    | "PRIORITY_HL";
+
+type OrderNode = { type: OrderTasks; next: null | OrderNode };
+const orderNode = ((orderTypes: OrderTasks[]): OrderNode => {
+    const first = { type: orderTypes[0], next: null } as OrderNode;
+
+    let prev = first;
+
+    for (let i = 1; i < orderTypes.length; ++i) {
+        const next = { type: orderTypes[i], next: null };
+        prev.next = next;
+        prev = next;
+    }
+
+    prev.next = first;
+
+    return first;
+})(["NONE", "INC_AO", "DEC_AO", "PRIORITY_LH", "PRIORITY_HL"]);
 
 interface AppState {
     tasks: Task[];
@@ -10,7 +34,7 @@ interface AppState {
     addText: string;
     editText: string;
     idx: number;
-    order: "NONE" | "INC_AO" | "DEC_AO" | "PRIORITY_LH" | "PRIORITY_HL";
+    order: OrderNode;
 }
 
 export function useTasks(tasks: Tasks) {
@@ -20,7 +44,7 @@ export function useTasks(tasks: Tasks) {
         normal: true,
         addText: "",
         editText: "",
-        order: "NONE",
+        order: orderNode,
     };
 
     const [state, setState] = useState(initialState);
@@ -38,6 +62,10 @@ export function useTasks(tasks: Tasks) {
         if (state.normal) {
             if (input === "i" || input === "e") {
                 enterInsert();
+            }
+
+            if (input === "o") {
+                cycleOrder();
             }
 
             // navigation
@@ -93,6 +121,10 @@ export function useTasks(tasks: Tasks) {
         setText(newText, "EDIT");
     }
 
+    function cloneTasks(): Tasks {
+        return [...state.tasks.map((t) => ({ ...t }))];
+    }
+
     function handleEnterNormal(): void {
         // Last index is the add button, anything previos is task
         if (state.idx === state.tasks.length) {
@@ -112,6 +144,52 @@ export function useTasks(tasks: Tasks) {
         setState({ ...state, normal: true });
     }
 
+    function cycleOrder(): void {
+        const next: OrderNode = state.order.next!;
+
+        const newTasks = orderTasks(cloneTasks(), next.type);
+
+        setState({ ...state, order: next, tasks: newTasks });
+    }
+
+    function orderTasks(tasks: Tasks, orderType: OrderTasks): Tasks {
+        if (orderType === "INC_AO") {
+            return tasks.sort((a, b) => {
+                if (a.name < b.name) return -1;
+                if (a.name > b.name) return 1;
+                return 0;
+            });
+        }
+
+        if (orderType === "DEC_AO") {
+            return tasks.sort((a, b) => {
+                if (a.name < b.name) return 1;
+                if (a.name > b.name) return -1;
+                return 0;
+            });
+        }
+
+        const pMap = { low: 1, medium: 2, high: 3 } as const;
+        if (orderType === "PRIORITY_LH") {
+            return tasks.sort((a, b) => {
+                if (pMap[a.priority] < pMap[b.priority]) return -1;
+                if (pMap[a.priority] > pMap[b.priority]) return 1;
+                return 0;
+            });
+        }
+
+        if (orderType === "PRIORITY_HL") {
+            return tasks.sort((a, b) => {
+                if (pMap[a.priority] < pMap[b.priority]) return 1;
+                if (pMap[a.priority] > pMap[b.priority]) return -1;
+                return 0;
+            });
+        }
+
+        // if orderType === "NONE"
+        return tasks;
+    }
+
     async function changePriority(direction: "inc" | "dec"): Promise<void> {
         if (state.idx >= state.tasks.length) return;
 
@@ -125,11 +203,17 @@ export function useTasks(tasks: Tasks) {
             task.priority = task.priority === "high" ? "medium" : "low";
         }
 
-        const newTasks = await Fetch.sendReq("PUT", task);
+        const response = await Fetch.sendReq("PUT", task);
 
-        if (newTasks) {
-            setState({ ...state, tasks: newTasks });
-        }
+        if (!response) return;
+
+        const newTasks = cloneTasks();
+        newTasks[state.idx] = task;
+
+        setState({
+            ...state,
+            tasks: newTasks,
+        });
     }
 
     async function deleteTask(): Promise<void> {
@@ -137,9 +221,12 @@ export function useTasks(tasks: Tasks) {
 
         const newTasks = await Fetch.sendReq("DELETE", toDelete);
 
-        if (newTasks) {
-            setState({ ...state, tasks: newTasks });
-        }
+        if (!newTasks) return;
+
+        setState({
+            ...state,
+            tasks: orderTasks(newTasks, state.order.type),
+        });
     }
 
     async function editTask(): Promise<void> {
@@ -147,21 +234,24 @@ export function useTasks(tasks: Tasks) {
 
         const prev: Task = state.tasks[state.idx];
 
-        const task: Task = {
+        const newTask: Task = {
             id: prev.id,
             priority: prev.priority,
             name: state.editText,
         };
 
-        const newTasks = await Fetch.sendReq("PUT", task);
+        const response = await Fetch.sendReq("PUT", newTask);
 
-        if (!newTasks) return;
+        if (!response) return;
+
+        const newTasks = cloneTasks();
+        newTasks[state.idx] = newTask;
 
         setState({
             ...state,
-            tasks: newTasks,
             normal: true,
             editText: "",
+            tasks: newTasks,
         });
     }
 
@@ -180,9 +270,10 @@ export function useTasks(tasks: Tasks) {
 
         setState({
             ...state,
+            idx: ++state.idx, // keep focus on add button
             normal: true,
             addText: "",
-            tasks: newTasks,
+            tasks: orderTasks(newTasks, state.order.type),
         });
     }
 
